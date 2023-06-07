47af5f73b162500377dd18c5658473e98103df1f
Hugo Acosta <thenex@gmail.com>
2023-06-07 16:14:18 -0400

    Mapas funcionales listos

--- app/models/caravan.rb
+++ app/models/caravan.rb
@@ -2,5 +2,8 @@ class Caravan < ApplicationRecord
   has_one_attached :photo
   belongs_to :user
   has_many :reservations, dependent: :destroy
-  validates :brand, :model, :year, :capacity, :gas_type, :description, presence: true
+  validates :brand, :model, :year, :capacity, :gas_type, :description, :address, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
