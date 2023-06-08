6d09c083ecf356463f6010d70d3adec776038319
Hugo Acosta <thenex@gmail.com>
2023-06-08 17:21:42 -0400

    Validacion de fechas de reservas, hecho

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,15 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :caravan
+
+  validates :start_date, presence: true
+
+  before_save :date_verification
+
+  def date_verification
+    if start_date.present? && start_date < Date.current
+      errors.add(:start_date, "no puede ser anterior a la fecha actual")
+      throw(:abort)
+    end
+  end
 end
